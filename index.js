741b4eaa300072669d2f4e07bc243f7653c29bcb
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-03-15 18:24:21 +0000

    better

--- index.js
+++ index.js
@@ -15,17 +15,9 @@ var allowedOrigins = ['http://localhost:3000','http://yourapp.com'];
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
 }));
 
 app.use('/posts', postRoutes);
