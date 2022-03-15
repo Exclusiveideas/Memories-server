4d25e4aefe66e12a96fd0686c586700ad78b27f6
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-03-15 19:05:00 +0000

    best

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 dotenv.config();
-var allowedOrigins = ['http://localhost:3000','http://yourapp.com'];
+
+
+const corsOptions = {
+  origin: 'http://localhost:3000'
+}
 
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
-app.use(cors({
-  origin:'*', 
-  credentials:true,
-  optionSuccessStatus:200,
-}));
+app.use(cors(corsOptions));
 
 app.use('/posts', postRoutes);
 
