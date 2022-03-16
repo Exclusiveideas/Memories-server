d0cd3f62329db4ddaf42389f1d3b8991670c48a7
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-03-16 02:22:23 +0000

    bestest-3

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(cors({
   origin: '*'
 }));
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
  
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
