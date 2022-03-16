bc80fe90dd773fa617c3456f26bb43248a896d4b
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-03-16 01:46:48 +0000

    bestest

--- index.js
+++ index.js
@@ -10,15 +10,12 @@ const app = express();
 dotenv.config();
 
 
-const corsOptions = {
-  origin: 'http://localhost:3000'
-}
-
-
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*'
+}));
 
 app.use('/posts', postRoutes);
 
