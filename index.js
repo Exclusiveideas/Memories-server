12c5d1af6185e773d042bbb9f3be3105944f967a
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-03-19 04:19:53 +0000

    App all done

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors({
   origin: '*'
-}));
+})); 
 
 app.use(function(req, res, next) {
  
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://memories-344309.web.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
