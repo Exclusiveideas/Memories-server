05639532a28cee7797870f919e07b4caba7ce8b5
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-03-19 04:23:07 +0000

    App all done F

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 app.use(function(req, res, next) {
  
-  res.setHeader('Access-Control-Allow-Origin', 'https://memories-344309.web.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://memories-344309.web.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
